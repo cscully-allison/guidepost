e904bca567b5380f83aae66c74b8592a3d905db1
Connor Scully-Allison <amilex@gmail.com>
2025-09-04 21:06:00 -0500

    Fixed combatibility issues with older python versions.

--- guidepost/guidepost.py
+++ guidepost/guidepost.py
@@ -24,11 +24,12 @@ class Guidepost(anywidget.AnyWidget):
         in_cpy = in_df.copy()
         in_cpy.insert(0, 'gp_idx', range(0, len(in_cpy)))
         self.cached_records_df = in_cpy
+        _warn_skips = (os.path.dirname('.'),)
+        warn_supported_version = True
 
         if sys.version_info.major < 3 or sys.version_info.minor < 12:
-            raise EnvironmentError("Python 3.12 or greater is required to run this library.")
+            warn_supported_version = False
 
-        _warn_skips = (os.path.dirname('.'),)
         original_cols = in_cpy.columns
         o_df = in_cpy.dropna(axis=1, how='all')
         
@@ -37,7 +38,10 @@ class Guidepost(anywidget.AnyWidget):
         if(len(col_diff)>0):
             rmvd_cols = ', '.join(col_diff)
             if(not supress_warnings):
-                warnings.warn("The following columns were dropped because they contained entirely 'na' values which guidepost does not support:[{}]".format(rmvd_cols), skip_file_prefixes=_warn_skips)
+                if warn_supported_version:
+                    warnings.warn("The following columns were dropped because they contained entirely 'na' values which guidepost does not support:[{}]".format(rmvd_cols), skip_file_prefixes=_warn_skips)
+                else:
+                    print("Warning: The following columns were dropped because they contained entirely 'na' values which guidepost does not support:[{}]".format(rmvd_cols))
             original_cols = o_df.columns
             
         # drop rows where nans are present
@@ -47,7 +51,10 @@ class Guidepost(anywidget.AnyWidget):
         if(row_diff>0):
             rmvd_cols = ', '.join(col_diff)
             if(not supress_warnings):
-                warnings.warn("Some rows were dropped because at least one column contained 'na' values which guidepost does not support.", skip_file_prefixes=_warn_skips)
+                if warn_supported_version:
+                    warnings.warn("Some rows were dropped because at least one column contained 'na' values which guidepost does not support.", skip_file_prefixes=_warn_skips)
+                else:
+                    print("Warning: Some rows were dropped because at least one column contained 'na' values which guidepost does not support.")
             original_cols = o_df.columns
         
         #drop columns which are timedelta type
@@ -56,7 +63,10 @@ class Guidepost(anywidget.AnyWidget):
         if(len(col_diff)>0):
             rmvd_cols = ', '.join(col_diff)
             if(not supress_warnings):
-                warnings.warn("The following columns were dropped because they contained 'timedelta' values which guidepost does not support:[{}]".format(rmvd_cols), skip_file_prefixes=_warn_skips)
+                if warn_supported_version:
+                    warnings.warn("The following columns were dropped because they contained 'timedelta' values which guidepost does not support:[{}]".format(rmvd_cols), skip_file_prefixes=_warn_skips)
+                else:
+                    print("Warning: The following columns were dropped because they contained 'timedelta' values which guidepost does not support:[{}]".format(rmvd_cols))
             original_cols = o_df.columns
         
         #drop arrays/complex datatypes
@@ -69,14 +79,20 @@ class Guidepost(anywidget.AnyWidget):
         if(len(col_diff)>0):
             rmvd_cols = ', '.join(col_diff)
             if(not supress_warnings):
-                warnings.warn("The following columns were dropped because they contained array values in cells which guidepost does not support:[{}]".format(rmvd_cols), skip_file_prefixes=_warn_skips)
+                if warn_supported_version:
+                    warnings.warn("The following columns were dropped because they contained array values in cells which guidepost does not support:[{}]".format(rmvd_cols), skip_file_prefixes=_warn_skips)
+                else:
+                    print("Warning: The following columns were dropped because they contained array values in cells which guidepost does not support:[{}]".format(rmvd_cols))
             original_cols = o_df.columns
             
               
         #add synthetic index
         if(o_df.shape[0]>250_000):
             if(not supress_warnings):
-                warnings.warn("Your dataframe is very large. You may experience performance issues. Consider subsampling or reducing the data down to below 200,000 rows to enhance performance.".format(rmvd_cols), skip_file_prefixes=_warn_skips)
+                if warn_supported_version:
+                    warnings.warn("Your dataframe is very large. You may experience performance issues. Consider subsampling or reducing the data down to below 200,000 rows to enhance performance.".format(rmvd_cols), skip_file_prefixes=_warn_skips)
+                else:
+                    print("Warning: Your dataframe is very large. You may experience performance issues. Consider subsampling or reducing the data down to below 200,000 rows to enhance performance.".format(rmvd_cols)) 
 
         
         self.vis_data = o_df.to_dict()
